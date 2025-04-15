e15652bfbe4e93a81d48a1fde51994befbcc9134
tnew1131 <tnew1131@gmail.com>
2025-04-15 17:09:28 +0300

    second trial

---
+++ qq.py
@@ -0,0 +1,11 @@
+# برنامج بايثون بسيط لطباعه "مرحبا"
+
+طباعة الجملة
+print("مرحبا")
+
+print ('hhhhhhh')
+print ('hello world')
+print ("how old are you")
+print ("what is your name")
+print(" what are you doing write now")
+print ("for this point  ")
